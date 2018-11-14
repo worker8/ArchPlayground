3dff32056105efbc104fbddd54386f862c1a0ae9
Tan Jun Rong <tanjunrong@gmail.com>
2018-11-14 22:53:07 +0900

    1st commit

---
+++ app/src/main/java/github/com/worker8/archplayground/MainActivity.kt
@@ -0,0 +1,18 @@
+package github.com.worker8.archplayground
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nameInput.addTextChangedListener()
+    }
+}
+
+class Presenter {
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,35 @@
+object Versions {
+    val Tools = Tool()
+    val AndroidConfigs = AndroidConfig()
+
+    val rxJava = "2.1.13"
+    val rxAndroid = "2.0.2"
+    val rxBinding = "3.0.0-alpha1"
+    val fuel = "1.15.1"
+    val paging = "1.0.0"
+    val pagingRx = "1.0.0-rc1"
+    val archLifecycle = "1.1.1"
+    val room = "1.1.1"
+    val support = "28.0.0"
+    val retrofit = "2.4.0"
+    val constraintLayout = "1.1.3"
+    val junit = "4.12"
+    val testRunner = "1.0.2"
+    val espressoCore = "3.0.2"
+    val picasso = "2.71828"
+    val verticalViewPager = "19.0.1"
+    val threeTen = "1.1.1"
+}
+
+class Tool {
+    val kotlin = "1.3.0"
+    val buildGradle = "3.2.0"
+}
+
+class AndroidConfig {
+    val sdkVersion = 28
+    val minSdkVersion = 21
+    val versionCode = 1
+    val versionName = "1.0"
+
+}
