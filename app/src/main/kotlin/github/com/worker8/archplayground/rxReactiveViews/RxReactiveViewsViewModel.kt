73f1e0ed55a4c96cb4da6380d9f2e99f9e68fe79
Tan Jun Rong <tanjunrong@gmail.com>
2019-03-18 22:28:17 +0900

    make view reactive

--- app/src/main/kotlin/github/com/worker8/archplayground/rxReactiveViews/RxReactiveViewsViewModel.kt
+++ app/src/main/kotlin/github/com/worker8/archplayground/rxReactiveViews/RxReactiveViewsViewModel.kt
@@ -4,12 +4,13 @@ import androidx.lifecycle.*
 import com.worker8.redditapi.RedditApi
 import com.worker8.redditapi.model.t3_link.response.RedditLinkObject
 import github.com.worker8.archplayground.common.addTo
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlin.properties.Delegates
 
-class RxReactiveViewsViewModel : ViewModel(), LifecycleObserver {
+class RxReactiveViewsViewModel(val viewInput: RxReactiveViewsViewModel.ViewInput) : ViewModel(), LifecycleObserver {
     private val disposables = CompositeDisposable()
     var redditApi: RedditApi by Delegates.observable(RedditApi()) { property, oldValue, newValue ->
         screenState.postValue(listOf())
@@ -21,49 +22,47 @@ class RxReactiveViewsViewModel : ViewModel(), LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onCreate() {
-    }
-
-    fun refreshClick() {
-        redditApi = RedditApi(redditApi.subreddit)
-        loadMorePosts()
-    }
-
-    fun randomSubredditClick() {
-        redditApi = RedditApi(RedditApi.getRandomSubreddit())
-        loadMorePosts()
-    }
-
-    fun onLoadMoreClick() {
-        loadMorePosts()
-    }
-
-    fun loadMorePosts() {
-        redditApi.getMorePosts()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ (result, fuelError) ->
-                result?.value?.let { _list ->
-                    val newList = screenState.value?.toMutableList() ?: mutableListOf()
-                    newList.addAll(_list.getRedditImageLinks())
-                    screenState.postValue(newList)
-                }
-            }, {
-                it.printStackTrace()
-            })
-            .addTo(disposables)
+        viewInput.apply {
+            Observable.merge(
+                refreshClickObservable.doOnNext { redditApi = RedditApi(redditApi.subreddit) },
+                randomClickObservable.doOnNext { redditApi = RedditApi(RedditApi.getRandomSubreddit()) },
+                loadMoreClickObservable
+            )
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .observeOn(Schedulers.io())
+                .flatMap { redditApi.getMorePosts() }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ (result, fuelError) ->
+                    result?.value?.let { _list ->
+                        val newList = screenState.value?.toMutableList() ?: mutableListOf()
+                        newList.addAll(_list.getRedditImageLinks())
+                        screenState.postValue(newList)
+                    }
+                }, {
+                    it.printStackTrace()
+                })
+                .addTo(disposables)
+        }
     }
 
     override fun onCleared() {
         super.onCleared()
         disposables.dispose()
     }
+
+    interface ViewInput {
+        val randomClickObservable: Observable<Unit>
+        val refreshClickObservable: Observable<Unit>
+        val loadMoreClickObservable: Observable<Unit>
+    }
 }
 
 @Suppress("UNCHECKED_CAST")
-class RxReactiveViewsViewModelFactory() : ViewModelProvider.NewInstanceFactory() {
+class RxReactiveViewsViewModelFactory(val viewInput: RxReactiveViewsViewModel.ViewInput) : ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return RxReactiveViewsViewModel() as T
+        return RxReactiveViewsViewModel(viewInput) as T
     }
 }
+
 
 class RxReactiveViewsScreenState()
